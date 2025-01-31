724e5784754c560b6f6f03ddec792e189f4bd3f6
Jordan <pettyjohnjn@polaris-login-04.hsn.cm.polaris.alcf.anl.gov>
2025-01-31 18:10:40 +0000

    Enabled quantization --- Still not working.

--- attention_lens/model/get_model.py
+++ attention_lens/model/get_model.py
@@ -28,7 +28,7 @@ def get_model(
         bnb_4bit_compute_dtype=torch.float16  # Compute dtype during inference
     )
 
-    model = AutoModelForCausalLM.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name, quantization_config=quantization_config)
     # model.to(device)
 
     model.eval()
